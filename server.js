0aa4ac3d38d8fb305691001e867b5eae417832f4
Nitsss26 <99978200+Nitsss26@users.noreply.github.com>
2025-05-09 11:55:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -251,7 +251,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://raycom.vercel.app",
 }));
 app.use(express.json());
 
